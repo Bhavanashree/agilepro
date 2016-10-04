41a8fbdb06a2835a0fdcfe8174c6aecf1087b3d8
pritam285 <pritam@285>
2016-10-04 22:47:32 +0530

    Project Team ui

--- Webapp/src/main/webapp/ui/js/member/project-team.js
+++ Webapp/src/main/webapp/ui/js/member/project-team.js
@@ -3,7 +3,38 @@ $.application.controller('projectTeamController',
                         function($scope, crudController, actionHelper, utils, modelDefService, validator){
 	
 	$scope.projectTeams = [];
+	$scope.teamIdObjMap = {};	
+	
+	
+	$scope.fetchAllProjectTeam = function(){
+	
+		actionHelper.invokeAction("projectTeam.readByProjectId", null, {"projectId" : $scope.getActiveProjectId()}, function(readResponse, respConfig){
+			
+			$scope.projectTeams = readResponse.model;
+			$scope.selectedTeam = $scope.projectTeams[0]; 
+			
+			for(index in $scope.projectTeams)
+			{
+				$scope.teamIdObjMap[$scope.projectTeams[index].id] = $scope.projectTeams[index];
+			}
+			
+			try
+			{
+				$scope.$apply();
+			}catch(ex)
+			{}
+			
+		}, true);
 		
+		
+	};
+	
+	$scope.onTeamChange = function(teamId){
+		
+		$scope.selectedTeam = $scope.teamIdObjMap[teamId];
+	};
+	
+	
 	$scope.addNewTeam = function(){
 		
 		$scope.projectTeam = {};
@@ -15,6 +46,8 @@ $.application.controller('projectTeamController',
 	
 	$scope.saveTeam = function($event){
 		
+		$scope.projectTeam.projectId = $scope.getActiveProjectId();
+		
 		$scope.projectTeams.push($scope.projectTeam);
 		
 		$scope.selectedTeam = $scope.projectTeams[0]; 
@@ -23,11 +56,36 @@ $.application.controller('projectTeamController',
 			
 			if(readResponse.code != 0)
 			{
-				$scope.selectedTeam = {};
 				// fetch all
 			}
 			
 		}, true);
 	};
+	
+	$scope.deleteTeam = function(teamId){
+		
+		var deleteOp = $.proxy(function(confirmed) {
+			
+			if(!confirmed)
+			{
+				return;
+			}
+			else
+			{
+				actionHelper.invokeAction("projectTeam.delete", null, {"id" : $scope.teamIdObjMap[teamId].id}, deleteAttachmentCallBack);
+			}
+			
+			try
+			{
+				this.$scope.$parent.$digest();
+			}catch(ex)
+			{}
+			
+		}, {"$scope": $scope});
+
+		utils.confirm(["Are you sure you want to delete {} and there respective members ?", $scope.teamIdObjMap[teamId].name], deleteOp);
+
+	};
+	
 	
 }]);
