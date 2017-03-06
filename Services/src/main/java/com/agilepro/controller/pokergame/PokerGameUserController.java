b90f9d55b7c35575f2c13b045c094bc695cf8e78
pritam285 <pritamjosephjojo@gmail.com>
2017-03-06 17:31:27 +0530

    Poker game validate card value

--- Services/src/main/java/com/agilepro/controller/pokergame/PokerGameUserController.java
+++ Services/src/main/java/com/agilepro/controller/pokergame/PokerGameUserController.java
@@ -3,6 +3,10 @@ package com.agilepro.controller.pokergame;
 import static com.agilepro.commons.IAgileproActions.ACTION_PREFIX_POKER_GAME_USER;
 import static com.agilepro.commons.IAgileproActions.ACTION_TYPE_ON_CHANGE_CARD;
 import static com.agilepro.commons.IAgileproActions.ACTION_TYPE_SAVE;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +15,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.agilepro.commons.GameSeries;
 import com.agilepro.commons.UserRole;
+import com.agilepro.commons.models.pokergame.PokerGameModel;
 import com.agilepro.commons.models.pokergame.PokerGameUserModel;
 import com.agilepro.services.common.Authorization;
+import com.agilepro.services.pokergame.PokerGameService;
 import com.agilepro.services.pokergame.PokerGameUserService;
 import com.yukthi.webutils.annotations.ActionName;
 import com.yukthi.webutils.common.models.BaseResponse;
@@ -33,7 +41,28 @@ public class PokerGameUserController extends BaseController
 	 */
 	@Autowired
 	private PokerGameUserService pokerUserService;
+	
+	/**
+	 * Poker game service.
+	 */
+	@Autowired
+	private PokerGameService pokerGameService;
 
+	private List<Integer> fetchValues(int numberOfCards, GameSeries gameSeries)
+	{
+		return new ArrayList<>();
+	}
+	
+	/**
+	 * Validates whether the provided value is present in the series.
+	 */
+	private void validateValue(Long pokerGameId)
+	{
+		PokerGameModel pokerGameModel = pokerGameService.fetchFullModel(pokerGameId, PokerGameModel.class);
+		
+		this.fetchValues(pokerGameModel.getNumberOfCards(), pokerGameModel.getGameSeries());
+	}
+	
 	/**
 	 * Save new poker game user.
 	 * 
@@ -53,8 +82,9 @@ public class PokerGameUserController extends BaseController
 	@Authorization(roles = { UserRole.EMPLOYEE_VIEW, UserRole.EMPLOYEE_EDIT })
 	@RequestMapping(value = "/onChangeCard", method = RequestMethod.GET)
 	@ResponseBody
-	public BaseResponse onChangeCard(@RequestParam(value = "id") Long id, @RequestParam(value = "cardValueDisplay") String cardValueDisplay)
+	public BaseResponse onChangeCard(@RequestParam(value = "pokerGameId") Long pokerGameId, @RequestParam(value = "userId") Long userId, @RequestParam(value = "cardValueDisplay") String cardValueDisplay)
 	{
+		this.validateValue(pokerGameId);
 		return null;
 	}
 }
